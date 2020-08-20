e4ab9a0706e5a2332936a69bf0cee6448575cfb8
junghun5947 <junghun5947@gmail.com>
2020-08-21 00:45:33 +0900

    H2 설정 및 User class Entity 설정
    H2 url:
    http://localhost:8088/h2-console/

--- src/main/java/com/pjh/restfulwebservice/user/configuration/SecurityConfiguration.java
+++ src/main/java/com/pjh/restfulwebservice/user/configuration/SecurityConfiguration.java
@@ -3,11 +3,23 @@ package com.pjh.restfulwebservice.user.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 //@Configuration spring boot 기동하면서 memory에 할당
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    //H2 library를 사용하기위한 security 예외처리
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        //h2-console~ permition 통과
+        http.authorizeRequests().antMatchers("/h2-onsole/**").permitAll();
+        //cross side script 사용안함
+        http.csrf().disable();
+        //frame option 사용안함
+        http.headers().frameOptions().disable();
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1, sysdate(), 'User1', 'test111', '921029-1111111');
+insert into user values(2, sysdate(), 'User2', 'test222', '921029-1111112');
+insert into user values(3, sysdate(), 'User3', 'test333', '921029-1111113');
